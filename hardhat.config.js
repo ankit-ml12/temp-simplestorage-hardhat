95698cc547baf4cf20d3692620ab7d6ba7b92b22
ankit patidar <patiankit6555@gmail.com>
2022-12-04 17:15:58 +0530

    3ss

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY= process.env.GOERLI_PRIVATE_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.7",
+  networks:{
+    goerli:{
+      url: GOERLI_RPC_URL, // got from alchemy
+      accounts:[PRIVATE_KEY], // from wallet
+      chainId: 5, // search on google for your network
+    },
+  }
 };
