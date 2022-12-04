9186dedbf69ac1ffc9a560878a3bb166c67de80a
ankit patidar <patiankit6555@gmail.com>
2022-12-04 21:05:39 +0530

    8ss

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config()
 require('@nomiclabs/hardhat-etherscan')
 require('./tasks/block-number');
+require('hardhat-gas-reporter')
+
 // require('@nomiclabs/hardhat-etherscan')
 const PRIVATE_KEY= process.env.GOERLI_PRIVATE_KEY
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const ETHERSCANE_API_KEY = process.env.ETHERSCANE_API_KEY
+const COIN_MARKET_API_KEY = process.env.COIN_MARKET_API_KEY
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -24,5 +27,16 @@ module.exports = {
   },
   etherscan:{
     apiKey:ETHERSCANE_API_KEY, 
- }
+ },
+ gasReporter:{
+  enabled: true,
+  outputFile: "gas-report.txt",
+  noColors: true,
+  currency: "USD",
+  coinmarketcap: COIN_MARKET_API_KEY,
+  //// token : use for other network 
+// sign up https://pro.coinmarketcap.com/account/
+// to get api to convert gas into dollar
+// 1e1c69f1-3947-46cb-951f-e7d587cf22f0
+}
 };
